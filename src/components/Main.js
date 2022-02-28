53344391417d76273c8c83b1efece774eb7c4121
Rohit Parihar <75430820+Rohitparihar1182@users.noreply.github.com>
2022-02-28 07:19:03 +0530

    created_loading_comp

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ConvertData from './Util'
 import Question from './Question';
+import Loading from './Loading';
 
 export default function Main(){
     const [data, setData] = React.useState([])
     const [checked, setChecked] = React.useState(false)
     const [score, setScore] = React.useState(0)
+    const [isLoading, setIsLoading] = React.useState(true)
 
     async function fetchApi(){
         const response=await fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=easy')
         const apiData=await response.json()
-        setData(ConvertData(apiData.results));
+        setTimeout(()=>{
+            setData(ConvertData(apiData.results));
+            setIsLoading(false)
+        }, 3000)
     }
 
     React.useEffect(()=>{
@@ -44,39 +49,44 @@ export default function Main(){
             fetchApi()
             setChecked(false)
             setScore(0)
+            setIsLoading(true)
         }
     }
-
+    
     return (
         <div className="main flex">
             <div className="rectangle"></div>
-            <div className="main--content">
-                {data.map(item=>{
-                    return <Question 
-                        key={item.id}
-                        id={item.id}
-                        question={item.question} 
-                        options={item.options}
-                        selected={item.selected}
-                        selectAnswer={selectAnswer}
-                        correct_answer={item.correct_answer}
-                        checked={checked}
-                    />
-                })}
-                <div className="main--btn--container">
-                    <p
-                        style={checked?{marginRight:'20px'}:{}}
-                    >
-                        {checked?`You scored ${score}/5 correct answer`:''}
-                    </p>
-                    <button
-                        className="btn main--btn"
-                        onClick={checkAnswers}
-                    >
-                        {checked?"Play again":"Check answers"}
-                    </button>
+            {isLoading ? 
+                <Loading /> : 
+                <div className="main--content">
+                    {data.map(item=>{
+                        return <Question 
+                            key={item.id}
+                            id={item.id}
+                            question={item.question} 
+                            options={item.options}
+                            selected={item.selected}
+                            selectAnswer={selectAnswer}
+                            correct_answer={item.correct_answer}
+                            checked={checked}
+                        />
+                    })}
+                    <div className="main--btn--container">
+                        <p
+                            style={checked?{marginRight:'20px'}:{}}
+                        >
+                            {checked?`You scored ${score}/5 correct answer`:''}
+                        </p>
+                        <button
+                            className="btn main--btn"
+                            onClick={checkAnswers}
+                        >
+                            {checked?"Play again":"Check answers"}
+                        </button>
+                    </div>
                 </div>
-            </div>
+            }
+            
             <div className="circle"></div>
         </div>
     )
